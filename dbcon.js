6c5c6e72d0e2a99614895d7fb4b5d73c9b389976
JenCarrigan <jennifer.dianne.carrigan@gmail.com>
2018-05-22 14:46:34 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs290_carrigaj',
-  password        : '0176',
+  password        : 'xxxx',
   database        : 'cs290_carrigaj'
 });
 
